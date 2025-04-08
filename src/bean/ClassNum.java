f146d41785e63caeb106f3d83829ba169431226b
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-08 14:09:29 +0900

    Bean - ClassNum.java 作成/実装

---
+++ src/bean/ClassNum.java
@@ -0,0 +1,24 @@
+package bean;
+
+public class ClassNum implements java.io.Serializable {
+
+	private String schoolCD;
+	private int classNum;
+
+	public String getSchoolCD() {
+		return schoolCD;
+	}
+
+	public void setSchoolCD(String schoolCD) {
+		this.schoolCD = schoolCD;
+	}
+
+	public int getClassNum() {
+		return classNum;
+	}
+
+	public void setClassNum(int classNum) {
+		this.classNum = classNum;
+	}
+
+}
