1bfbf0ad73755f3be842747086f8184cfd6eaa64
'a-oba4485' <'fko2447060@stu.ac.jp'>
2025-04-08 14:11:53 +0900

    bean/Subject.java 作成

---
+++ src/bean/Subject.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class Subject implements java.io.Serializable {
+
+	private String shoolCD;
+	private String CD;
+	private String name;
+
+	public String getShoolCD() {
+		return shoolCD;
+	}
+	public void setShoolCD(String shoolCD) {
+		this.shoolCD = shoolCD;
+	}
+	public String getCD() {
+		return CD;
+	}
+	public void setCD(String cD) {
+		CD = cD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
