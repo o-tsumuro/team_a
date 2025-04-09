2b9d3c2da061916711d145bda363f00bfc3b2c0b
y-ooe <fko2447054@stu.o-hara.ac.jp>
2025-04-09 14:15:43 +0900

    add:ClassNumDAO

--- src/dao/ClassNumDAO.java
+++ src/dao/ClassNumDAO.java
@@ -1,39 +1,35 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import bean.ClassNum;
+import bean.School;
+
 public class ClassNumDAO extends DAO {
 
-//	public ClassNum get(String class_num, School school) throws Exception {
-//		Connection con = getConnection();
-//
-//		PreparedStatement st = con.prepareStatement(
-//				"SELECT * FROM CLASS_NUM WHERE SCHOOL_CD = ? AND CLASS_NUM = ? ");
-//		st.setString(1, school.getCd());
-//		st.setString(2, class_num.);
-//	}
-//
-//
-//    public List<ClassNum> getAllStudents() throws Exception {
-//        // TODO: 実装
-//    	List<ClassNum> list = new ArrayList<>();
-//    	Connection con = getConnection();
-//
-//    	PreparedStatement st = con.prepareStatement(
-//    			"SELECT student_id, student_name, STUDENT.course_id, course_name FROM STUDENT JOIN COURSE ON STUDENT.course_id = COURSE.course_id");
-//    	ResultSet rs = st.executeQuery();
-//
-//    	while(rs.next()){
-//			Student stu = new Student();
-//			stu.setId(rs.getInt("student_id"));
-//			stu.setName(rs.getString("student_name"));
-//			stu.setCourseId(rs.getInt("course_id"));
-//			stu.setCourseName(rs.getString("course_name"));
-//			list.add(stu);
-//    	}
-//
-//    	st.close();
-//    	con.close();
-//
-//        return list;
-//    }
+	public ClassNum get(String classnum, School school) throws Exception {
+		Connection con = getConnection();
+
+		PreparedStatement st = con.prepareStatement(
+				"SELECT * FROM CLASS_NUM WHERE SCHOOL_CD = ? AND CLASS_NUM = ? ");
+		st.setString(1, school.getCd());
+		st.setString(2, classnum);
+
+		ResultSet rs = st.executeQuery();
+
+		ClassNum cn = new ClassNum();
+		cn.setClassNum(Integer.parseInt(rs.getString("class_num")));
+		cn.setSchoolCd(rs.getString("school_cd"));
+
+
+    	st.close();
+    	con.close();
+
+    	return cn;
+
+	}
+
 
 }
