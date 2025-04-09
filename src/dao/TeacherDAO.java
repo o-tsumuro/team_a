8e271a865f8129186e81cd47cbf26f0758c4213c
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-09 14:29:05 +0900

    dao/TeacherDAO - login機能実装

--- src/dao/TeacherDAO.java
+++ src/dao/TeacherDAO.java
@@ -1,5 +1,34 @@
 package dao;
 
-public class TeacherDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import bean.Teacher;
+
+public class TeacherDAO extends DAO{
+
+	public Teacher login(String id, String password) throws Exception {
+		Teacher teacher=null;
+
+		Connection con=getConnection();
+
+		PreparedStatement st;
+		st=con.prepareStatement("select * from teacher where id=? and password=?");
+		st.setString(1, id);
+		st.setString(2, password);
+		ResultSet rs=st.executeQuery();
+
+		while (rs.next()) {
+			teacher=new Teacher();
+			teacher.setId(rs.getString("id"));
+			teacher.setName(rs.getString("name"));
+			teacher.setPassword(rs.getString("password"));
+			teacher.setSchoolCd(rs.getString("school_cd"));
+		}
+
+		st.close();
+		con.close();
+		return teacher;
+	}
 }
