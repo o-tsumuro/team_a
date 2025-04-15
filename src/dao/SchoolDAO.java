527cb3a8b06b1a3a09f7b424e8ecd0c69848d922
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-15 15:07:52 +0900

    SchoolDAO.java getSchool関数実装(ログイン時にSchoolを得るため)

--- src/dao/SchoolDAO.java
+++ src/dao/SchoolDAO.java
@@ -1,5 +1,32 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import bean.School;
+
 public class SchoolDAO extends DAO {
 
+	 public School getSchool(String cd) throws Exception {
+	        School school = null;
+
+	        Connection con = getConnection();
+	        PreparedStatement st = con.prepareStatement("SELECT * FROM school WHERE cd = ?");
+	        st.setString(1, cd);
+
+	        ResultSet rs = st.executeQuery();
+
+	        if (rs.next()) {
+	            school = new School();
+	            school.setCd(rs.getString("cd"));
+	            school.setName(rs.getString("name"));
+	        }
+
+	        rs.close();
+	        st.close();
+	        con.close();
+
+	        return school;
+	    }
 }
