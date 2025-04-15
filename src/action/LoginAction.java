99016faf63be76e77da80ecbd83e8b4437d3a059
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-15 15:08:36 +0900

    LoginAction.java リファクタリング + Schoolをセッションに追加

--- src/action/LoginAction.java
+++ src/action/LoginAction.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.Teacher;
+import dao.SchoolDAO;
 import dao.TeacherDAO;
 import tool.Action;
 
@@ -12,22 +13,26 @@ public class LoginAction extends Action{
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		HttpSession session=req.getSession();
+		HttpSession session = req.getSession();
 
-
-		String id=req.getParameter("id");
+		String id = req.getParameter("id");
 		String password=req.getParameter("password");
-		TeacherDAO dao=new TeacherDAO();
-		Teacher teacher=dao.login(id, password);
 
-		 if (teacher != null) {
-		        session.setAttribute("teacher", teacher);  
-		        session.setAttribute("user_school_cd", teacher.getSchoolCd()); 
-		        System.out.println("success");
-		        return "/index.jsp";  
-		    }
+		TeacherDAO teacherDao = new TeacherDAO();
+		Teacher teacher = teacherDao.login(id, password);
 
-		return "#";
+		if (teacher == null) {
+			System.out.println("ログイン失敗");
+			return "#";
+		}
+
+		session.setAttribute("teacher", teacher);
+		session.setAttribute("user_school_cd", teacher.getSchoolCd());
+
+		SchoolDAO schoolDao = new SchoolDAO();
+		schoolDao.getSchool(teacher.getSchoolCd());
+
+		return "/index.jsp";
 	}
 
 }
