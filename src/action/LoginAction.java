c824320f16bd1be0a76eec700410c240fca0c3d1
'a-oba4485' <'fko2447060@stu.ac.jp'>
2025-04-15 13:48:17 +0900

    schoolCdを取得する為にif内修正

--- src/action/LoginAction.java
+++ src/action/LoginAction.java
@@ -14,16 +14,22 @@ public class LoginAction extends Action{
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		HttpSession session=req.getSession();
 
+
 		String id=req.getParameter("id");
 		String password=req.getParameter("password");
 		TeacherDAO dao=new TeacherDAO();
 		Teacher teacher=dao.login(id, password);
 
-		if (teacher != null) {
-			session.setAttribute("teacher", teacher);
-			System.out.println("success");
-			return "/index.jsp";
-		}
+		 if (teacher != null) {
+		        // ログイン成功時
+		        session.setAttribute("teacher", teacher);  // teacher オブジェクトをセッションに保存
+
+		        // ここで teacher オブジェクトから schoolCd を取り出し、セッションに保存
+		        session.setAttribute("user_school_cd", teacher.getSchoolCd());  // user_school_cd をセッションに保存
+
+		        System.out.println("success");
+		        return "/index.jsp";  // ログイン後の遷移先
+		    }
 
 		return "#";
 	}
