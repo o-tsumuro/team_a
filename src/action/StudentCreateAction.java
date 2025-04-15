ee6076d4ecaf227a8f76cc768887bc4d75481691
'a-oba4485' <'fko2447060@stu.ac.jp'>
2025-04-15 11:55:46 +0900

    StudentCreateAction.javaの作成

---
+++ src/action/StudentCreateAction.java
@@ -0,0 +1,41 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Student;
+import dao.StudentDAO;
+import tool.Action;
+
+public class StudentCreateAction extends Action {
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try {
+            int no = Integer.parseInt(request.getParameter("no"));
+            String name = request.getParameter("name");
+            int year = Integer.parseInt(request.getParameter("ent_year"));
+            int classNum = Integer.parseInt(request.getParameter("class_num"));
+
+            Student student = new Student();
+            student.setNo(no);
+            student.setName(name);
+            student.setEntYear(year);
+            student.setClassNum(classNum);
+
+            StudentDAO dao = new StudentDAO();
+            boolean result = dao.save(student);
+
+            if (result) {
+                request.setAttribute("message", "学生情報が正常に登録されました！");
+            } else {
+                request.setAttribute("message", "学生情報の登録に失敗しました。");
+            }
+
+            return "result.jsp";
+
+        } catch (Exception e) {
+
+            System.out.println( "エラーが発生しました: " + e.getMessage());
+            return "error.jsp";
+        }
+    }
+}
