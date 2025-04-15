17137cb2bfac5457ce3ebf3c50809ff36eed3d5e
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-15 23:08:22 +0900

    login用サーブレット/jsp 修正

--- src/action/LoginAction.java
+++ src/action/LoginAction.java
@@ -2,40 +2,14 @@ package action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import bean.School;
-import bean.Teacher;
-import dao.SchoolDAO;
-import dao.TeacherDAO;
 import tool.Action;
 
 public class LoginAction extends Action{
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		HttpSession session = req.getSession();
-
-		String id = req.getParameter("id");
-		String password=req.getParameter("password");
-
-		TeacherDAO teacherDao = new TeacherDAO();
-		Teacher teacher = teacherDao.login(id, password);
-
-		if (teacher == null) {
-			System.out.println("ログイン失敗");
-			return "#";
-		}
-
-		session.setAttribute("teacher", teacher);
-		session.setAttribute("user_school_cd", teacher.getSchoolCd());
-
-		School school;
-		SchoolDAO schoolDao = new SchoolDAO();
-		school = schoolDao.getSchool(teacher.getSchoolCd());
-		session.setAttribute("school", school);
-
-		return "/index.jsp";
+		return "/auth/login.jsp";
 	}
 
 }
