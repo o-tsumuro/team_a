33effd364ac85c86a4bfbf065230e0b9a76a39e8
'a-oba4485' <'fko2447060@stu.ac.jp'>
2025-04-15 13:49:46 +0900

    インデント修正

--- src/action/LoginAction.java
+++ src/action/LoginAction.java
@@ -21,14 +21,10 @@ public class LoginAction extends Action{
 		Teacher teacher=dao.login(id, password);
 
 		 if (teacher != null) {
-		        // ログイン成功時
-		        session.setAttribute("teacher", teacher);  // teacher オブジェクトをセッションに保存
-
-		        // ここで teacher オブジェクトから schoolCd を取り出し、セッションに保存
-		        session.setAttribute("user_school_cd", teacher.getSchoolCd());  // user_school_cd をセッションに保存
-
+		        session.setAttribute("teacher", teacher);  
+		        session.setAttribute("user_school_cd", teacher.getSchoolCd()); 
 		        System.out.println("success");
-		        return "/index.jsp";  // ログイン後の遷移先
+		        return "/index.jsp";  
 		    }
 
 		return "#";
