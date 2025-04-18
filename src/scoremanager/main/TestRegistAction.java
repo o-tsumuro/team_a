2588e37c44c3c5ba4889bd1885b68cec088bb381
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-18 15:40:15 +0900

    TestRegistAction.java 実装途中

--- src/scoremanager/main/TestRegistAction.java
+++ src/scoremanager/main/TestRegistAction.java
@@ -4,17 +4,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.School;
 import bean.Teacher;
+import dao.ClassNumDAO;
+import dao.SubjectDAO;
 import tool.Action;
 
 public class TestRegistAction extends Action {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		HttpSession session=req.getSession(false);
-		@SuppressWarnings("unused")
-		Teacher teacher= (Teacher) session.getAttribute("teacher");
-		return "#";
+		HttpSession session = req.getSession();
+
+		Teacher teacher = (Teacher) session.getAttribute("teacher");
+		School school = (School) session.getAttribute("school");
+
+		ClassNumDAO classNumDao = new ClassNumDAO();
+		SubjectDAO subjectDao = new SubjectDAO();
+
+		session.setAttribute("classList", classNumDao.filter(school));
+		session.setAttribute("subjectList", subjectDao.filter(school));
+
+		return "/test/testRegist.jsp";
 	}
 
 }
