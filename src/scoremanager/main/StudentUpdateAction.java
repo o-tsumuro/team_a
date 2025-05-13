a0a01bf132b82197c70c9b4f37e5cde92d6bf74d
'a-oba4485' <'fko2447060@stu.ac.jp'>
2025-05-13 10:35:39 +0900

    classNumdaoオブジェクトの追加

--- src/scoremanager/main/StudentUpdateAction.java
+++ src/scoremanager/main/StudentUpdateAction.java
@@ -1,21 +1,34 @@
 package scoremanager.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.School;
 import bean.Student;
+import dao.ClassNumDAO;
 import dao.StudentDAO;
 import tool.Action;
 
 public class StudentUpdateAction extends Action {
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String no = request.getParameter("no");
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession(false);
+		School school = (School) session.getAttribute("school");
 
-        StudentDAO dao = new StudentDAO();
-        Student student = dao.get(no);
+		String no = request.getParameter("no");
+		StudentDAO studentDao = new StudentDAO();
+		Student student = studentDao.get(no);
 
-        request.setAttribute("student", student);
+		List<String> classNumList = new ArrayList<>();
+		ClassNumDAO classNumDao = new ClassNumDAO();
 
-        return "../student/student_update.jsp";
-    }
+		classNumList = classNumDao.filter(school);
+		request.setAttribute("classNumList", classNumList);
+		request.setAttribute("student", student);
+
+		return "../student/student_update.jsp";
+	}
 }
